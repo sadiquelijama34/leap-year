73328196fb4ac7f4e47cfb173b8581fdc7dc4c95
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-07 15:43:25 +0300

    added code that returns its a leap year for true and its not a leap year for false

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tittle  
+Leap year
+## Description  
+The project detects leap years displayed on a webpage  
+### Specifications  
+-Created a code to check whether provided year is divisible by 4  
+-Added another code to check whether provided year is divisible by 100  
+-Added another code to check whether year is divisible by 400  
+#### Copyright  
+Sadique Ali &copy 2017
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-$(document).ready(function() {
-    $("form#leap-year").submit(function(event) {
-      event.preventDefault();
-      var year = parseInt($("input#year").val());
-      var result = leapYear(year);
-      $("#result").text(result);
-    });
-  });
+
+// business logic
   var leapYear = function(year) {
     return false;
   };
@@ -17,9 +11,27 @@ $(document).ready(function() {
     }
   };
   var leapYear = function(year) {
-  if ((year % 4===0) && (year % 100 !==0) || (year % 400===0)){
+  if ((year % 4===0) && (year % 100 !==0) || (year % 400===0)) {
     return true;
   } else {
     return false;
   }
 };
+// user interface logic
+ $(document).ready(function() {
+   $("form#leap-year").submit(function(event) {
+     event.preventDefault();
+     var year = parseInt($("input#year").val());
+     var result = leapYear(year);
+
+     $(".year").text(year);
+
+     if (!result) {                 // same as writing if (result === false)
+       $(".not").text("not");
+     } else {
+       $(".not").text("");
+     }
+
+     $("#result").show();
+   });
+ });
