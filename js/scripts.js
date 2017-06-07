de8914e7ba9091d2f2a494fd8b4d28c25f15f13d
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-07 10:08:06 +0300

    added code to check whether provided year is divisible by 100

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,12 @@ $(document).ready(function() {
       return false;
     }
   };
+  var leapYear = function(year) {
+    if (year % 100===0){
+      return false;
+    } else if (year % 4===0){
+      return true;
+    } else {
+      return false;
+    }
+  };
