154182d767a142b0ba52e601125f482f805c52d8
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-07 10:38:31 +0300

    added code to check whether provided year is divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,9 @@ $(document).ready(function() {
     }
   };
   var leapYear = function(year) {
-    if (year % 100===0){
-      return false;
-    } else if (year % 4===0){
-      return true;
-    } else {
-      return false;
-    }
-  };
-var leapYear = function(year) {
-  if ((year % 4===0) && (year % 100 !==0)){
+  if ((year % 4===0) && (year % 100 !==0) || (year % 400===0)){
     return true;
   } else {
     return false;
   }
-};  
+};
