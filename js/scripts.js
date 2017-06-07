2b28264e30a09dad59a9962f645cd38245b278cd
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-07 10:00:14 +0300

    added code to check whether provided year is divisible by 4

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,10 @@ $(document).ready(function() {
   var leapYear = function(year) {
     return false;
   };
+  var leapYear = function(year) {
+    if (year % 4===0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
